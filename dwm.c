39ee9897b74e1e410c3cfa6c55b61393ae935115
christian poß <christianposs@gmx.de>
2021-01-07 15:30:23 +0100

    added title again

--- dwm.c
+++ dwm.c
@@ -99,7 +99,7 @@ enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
 enum { Manager, Xembed, XembedInfo, XLast }; /* Xembed atoms */
 enum { WMProtocols, WMDelete, WMState, WMTakeFocus, WMLast }; /* default atoms */
 enum { ClkTagBar, ClkLtSymbol, ClkStatusText, ClkClientWin,
-       ClkRootWin, ClkLast }; /* clicks */
+       ClkWinTitle, ClkRootWin, ClkLast }; /* clicks */
 
 typedef union {
 	int i;
@@ -601,26 +601,27 @@ buttonpress(XEvent *e)
 			arg.ui = 1 << i;
 		} else if (ev->x < x + blw)
 			click = ClkLtSymbol;
-		else {
-
+    else if (ev->x > (x = selmon->ww - TEXTWM(stext) + lrpad)) {
 			click = ClkStatusText;
-      char *text = rawstext;
-      int i = -1;
-      char ch;
-      dwmblockssig = 0;
-      while (text[++i]) {
-        if ((unsigned char)text[i] < '\x10') {
-          ch = text[i];
-          text[i] = '\0';
-          x += TEXTW(text) - lrpad;
-          text[i] = ch;
-          text += i+1;
-          i = -1;
-          if (x >= ev->x) break;
-          dwmblockssig = ch;
-        }
-      }
-		}
+
+			char *text = rawstext;
+			int i = -1;
+			char ch;
+			dwmblockssig = 0;
+			while (text[++i]) {
+				if ((unsigned char)text[i] < ' ') {
+					ch = text[i];
+					text[i] = '\0';
+					x += TEXTW(text) - lrpad;
+					text[i] = ch;
+					text += i+1;
+					i = -1;
+					if (x >= ev->x) break;
+					dwmblockssig = ch;
+				}
+			}
+		} else
+			click = ClkWinTitle;
 	} else if ((c = wintoclient(ev->window))) {
 		focus(c);
 		restack(selmon);
@@ -961,8 +962,8 @@ drawbar(Monitor *m)
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
     
-    if(showsystray && m == systraytomon(m))
-        stw = getsystraywidth();
+  if(showsystray && m == systraytomon(m))
+    stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
@@ -998,8 +999,15 @@ drawbar(Monitor *m)
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0, False);
 
 	if ((w = m->ww - sw - stw - x) > bh) {
-		drw_setscheme(drw, scheme[SchemeNorm]);
-		drw_rect(drw, x, 0, w, bh, 1, 1);
+		if (m->sel) {
+ 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0, False);
+			if (m->sel->isfloating)
+				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
+		} else {
+      drw_setscheme(drw, scheme[SchemeNorm]);
+		  drw_rect(drw, x, 0, w, bh, 1, 1);
+    }
 	}
 	drw_map(drw, m->barwin, 0, 0, m->ww - stw, bh);
 }
@@ -1550,8 +1558,11 @@ propertynotify(XEvent *e)
 			drawbars();
 			break;
 		}
-		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName])
+		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName]) {
 			updatetitle(c);
+	    if (c == c->mon->sel)
+				drawbar(c->mon);
+		}
 		if (ev->atom == netatom[NetWMWindowType])
 			updatewindowtype(c);
 	}
