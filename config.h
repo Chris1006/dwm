105c4c97314d481376bd76d87e19cb1e02845fdd
christian poß <christianposs@gmx.de>
2021-02-11 09:45:38 +0100

    changed color theme

--- config.h
+++ config.h
@@ -22,29 +22,16 @@ static const int sidepad            = 5;       /* horizontal padding of bar */
 static const char font[]            = "monospace 11";
 static const char dmenufont[]       = "monospace:size=11";
 
-static const char norm_fg[] = "#88C0D0";
-static const char norm_bg[] = "#2E3440";
-static const char norm_border[] = "#4C566A";
-
-static const char bar_border[] = "#4C566A";
-
-static const char sel_fg[] = "#D08770";
-static const char sel_bg[] = "#4C566A";
-static const char sel_border[] = "#8FBCBB";
-
-static const char urg_fg[] = "#8FBCBB";
-static const char urg_bg[] = "#88C0D0";
-static const char urg_border[] = "#88C0D0";
-
-/* static const char status_fg[] = "#B48EAD"; */
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#2e3440";
+static const char col_gray3[]       = "#81a1c1";
+static const char col_gray4[]       = "#e5e9f0";
+static const char col_cyan[]        = "#4c566a";
 
 static const char *colors[][3]      = {
-
     /*               fg           bg         border                         */
-    [SchemeNorm]    = { norm_fg,  norm_bg,   norm_border }, // unfocused wins
-    [SchemeSel]     = { sel_fg,   sel_bg,    sel_border },  // the focused win
-  //  [SchemeUrg]     = { urg_fg,   urg_bg,    urg_border },
- /*   [SchemeStatus]  = { status_fg,   norm_bg,   sel_fg }, // Statusbar right {text,background,not used but cannot be empty} */
+    [SchemeNorm]    = { col_gray3,  col_gray1,   col_gray1 }, // unfocused wins
+    [SchemeSel]     = { col_gray4,  col_cyan,    col_gray4 },  // the focused win
 }; 
 
 static const unsigned int baralpha = 0xd8;
